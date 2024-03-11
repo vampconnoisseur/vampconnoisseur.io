2d7b8276535b7f7abfa70c01485ece6da4a8aea5
Jaiditya Batra <jaiditya7318@gmail.com>
2024-03-11 23:31:13 +0530

    Revert "fix: Side Navigator"
    This reverts commit 986d50177bcd8bdbb38de1b555652157775dbcf5.
    
    fix: Side navigator placement in main file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./ui/navbar";
 import Footer from "./ui/footer/Footer";
-import SideNavigator from "./ui/side-navigator";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +20,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} text-black`}>
         <Navbar />
-        <SideNavigator sections={3} />
         <div className="max-w-screen-xl mx-auto md:px-12">{children}</div>
         <div className="mx-auto bg-black">
           <Footer />
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,7 @@
 import Hero from "./ui/Hero";
 import Skills from "./ui/skills/Skills";
 import { CardsSkeleton } from "./ui/skeletons";
+import SideNavigator from "./ui/side-navigator";
 
 export default function Home() {
   return (
@@ -10,13 +11,14 @@ export default function Home() {
         <div className="flex items-center mb-12 text-center px-6">
           <hr className="flex-grow border-t border-black border-solid" />
           <h2 className="text-xl md:text-3xl font-bold mx-4">
-            All of my projects
+            Some of my projects
           </h2>
           <hr className="flex-grow border-t border-black border-solid" />
         </div>
         <CardsSkeleton />
       </section>
       <Skills />
+      <SideNavigator sections={3} />
     </main>
   );
 }
